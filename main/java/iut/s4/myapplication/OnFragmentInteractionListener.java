1b56362444a0c7a434cd040c9387d5f2a2cde943
Julien_D <77330810+IkeaPencill@users.noreply.github.com>
2022-04-03 21:55:51 +0200

    Delete main/java/iut/s4/myapplication directory

--- main/java/iut/s4/myapplication/OnFragmentInteractionListener.java
+++
@@ -1,5 +0,0 @@
-package iut.s4.myapplication;
-
-public interface OnFragmentInteractionListener {
-    public void changeFragment(int id);
-}
--- main/java/iut/s4/myapplication/OnHeadlineSelectedListener.java
+++
@@ -1,5 +0,0 @@
-package iut.s4.myapplication;
-
-public interface OnHeadlineSelectedListener {
-        public void onDataSend(int position,String s);
-}
